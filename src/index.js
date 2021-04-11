4f13af1b578cb15e8a2b9479d73e53ef1048f3e5
Raphael Cassio de Souza <rapha.cassio@gmail.com>
2021-04-10 21:31:03 -0300

    Error handling configuration

--- src/index.js
+++ src/index.js
@@ -1,5 +1,6 @@
 import express from 'express';
 import 'express-async-errors';
+
 import routes from './routes';
 
 const app = express();
@@ -7,6 +8,10 @@ const port = 3000;
 
 app.use(express.json());
 app.use(routes);
+
+app.use(( _, __, res, ___ ) => {
+	return res.status(500).json({ error: 'Internal server error' });
+})
 
 app.listen(port, () => {
 	console.log(`Server listen on port ${port}`);
