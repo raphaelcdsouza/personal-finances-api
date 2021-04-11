6661dfb6390b3db6f7049428b535595f5a2333cc
Raphael Cassio de Souza <rapha.cassio@gmail.com>
2021-04-10 21:31:23 -0300

    Health check route

--- src/routes.js
+++ src/routes.js
@@ -1,9 +1,17 @@
 import { Router } from 'express';
 
+import db from './database';
+
 const routes = Router();
 
-routes.get('/', (_, res) => {
-	res.send('Hellow from outside router');
+routes.get('/v1/health', async (_, res) => {
+	const check = await db.raw('SELECT NOW();');
+
+	if (check.rowCount === 0) {
+		return res.status(500).json({ error: 'Cannot connect with database!' });
+	}
+
+	return res.json({ message: 'Connect sucessfully with database!' });
 });
 
 export default routes;
